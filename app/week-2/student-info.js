f4834d9a336939d176f8224c901447b91f12fbf0
Umaya Hewaarachchi <umayaush@gmail.com>
2024-09-24 22:50:42 -0600

    Created a week-2 folder, new page, and StudentInfo components

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>
+                Umaya Hewaarachchi
+            </h1>
+            <Link href="https://github.com/umayaush/cprg306-assignments.git">
+                Link to GitHub Repository
+            </Link>
+        </main>
+    );
+}
